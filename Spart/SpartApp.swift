d41b8b8b53bf8755076a29aa8b335ef1a49cfec9
Romi <romi.peli@pm.me>
2023-01-19 20:07:41 +0100

    Initial Commit

---
+++ Spart/SpartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpartApp.swift
+//  Spart
+//
+//  Created by Romi on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
